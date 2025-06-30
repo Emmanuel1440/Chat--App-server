3932955969598118a56a0943e78137fa413adce3
Emmanuel Wanjala <starrise1440@gmail.com>
2025-06-30 21:10:10 +0300

    Fix: use DB_URL for database connection

--- server/db.js
+++ server/db.js
@@ -1,13 +1,8 @@
-// server/db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5432,
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
