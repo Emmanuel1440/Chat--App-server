e3dc529362b5d14e10737132d4f0981f9c01bf8a
Emmanuel Wanjala <starrise1440@gmail.com>
2025-07-01 08:43:29 +0300

    Fix: add working db configs for local and render

--- server/db.js
+++ server/db.js
@@ -2,7 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL, // ✅ use this for Render
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
